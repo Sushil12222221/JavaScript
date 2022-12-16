8760b76704ada68df2fa158625c362d08a7fbe73
Sushil Kumar Verma <114611314+Sushil12222221@users.noreply.github.com>
2022-12-16 11:06:59 +0530

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,21 @@
+# This is a JavaScript program to find sum of two numbers
+<!DOCTYPE html>
+<html>
+<body>
+
+<p id = "abc"></p>
+<p id = "ab"></p>
+<p id = "a"></p>
+
+
+
+<script>
+
+var x = 4;
+var y = 5;
+document.write(x+y);
+
+</script>
+
+</body>
+</html>
